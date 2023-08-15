668443afac74ba157a7a44fe0134585c68b518e2
CaioLoyer <ricartecaio338@gmail.com>
2023-08-15 16:14:08 -0400

    chamando a lista

--- src/app/pages/dashboard/page.js
+++ src/app/pages/dashboard/page.js
@@ -5,7 +5,7 @@ export default async function Dashboard() {
     return (
         <div>
             <h1>Dashboard</h1>
-            
+            <ul><li>{getUsers}</li></ul>
         </div>
     );
 };
