1a2d9a5715318f3599fb6f34c0ba2e5f88f8b1c6
CaioLoyer <ricartecaio338@gmail.com>
2023-08-15 16:13:55 -0400

    lista criada

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -1,4 +1,24 @@
 'use server'
+const users = [
+    {
+      name: 'Leo',
+      email: 'leo@gmail.com',
+      password: 'senhadaora',
+      token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+    },
+    {
+      name: 'Vini',
+      email: 'vini@gmail.com',
+      password: 'senhadaora',
+      token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+    },
+    {
+        name: 'Costa',
+        email: 'costa@gmail.com',
+        password: 'senhadaora',
+        token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+    }
+  ];
 
   const getUserAuthenticated = (email, password) => 
   {
